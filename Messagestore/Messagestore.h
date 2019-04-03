25beb6ac11e26d3708fe607f82d2d57438d460fa
1amageek <tmy0x3@icloud.com>
2019-04-03 12:38:49 +0900

    Initial Commit

---
+++ Messagestore/Messagestore.h
@@ -0,0 +1,19 @@
+//
+//  Messagestore.h
+//  Messagestore
+//
+//  Created by 1amageek on 2019/04/03.
+//  Copyright © 2019 Stamp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Messagestore.
+FOUNDATION_EXPORT double MessagestoreVersionNumber;
+
+//! Project version string for Messagestore.
+FOUNDATION_EXPORT const unsigned char MessagestoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Messagestore/PublicHeader.h>
+
+
